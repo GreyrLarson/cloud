04c3563de9b37eda8684080024a35739c0de1c90
Grey Larson <greyrl@byu.edu>
2021-06-16 00:56:30 -0600

    catch-all route added

---
+++ src/views/NotFound.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="404">
+    <h1>404 Error</h1>
+    <h3>Something went wrong</h3>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+#404 {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+}
+</style>
